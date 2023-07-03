3bec97538106cf8714eb7d887b9bc94142c3d0d5
bigacz <sbigacz@gmail.com>
2023-07-03 20:05:01 +0200

    Change book layouts, add book's id

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const mainNode = document.querySelector('main')
-const addButton = document.getElementById('addButton')
+const mainNode = document.querySelector('main');
+const addButton = document.getElementById('add-button');
+const addForm = document.getElementById('form-add');
+const page = document.getElementById('page');
+
+addForm.style.display = "none";
+
+addButton.addEventListener('click', (e) => {
+  addForm.style.display = "block";
+  page.disabled = true;
+})
 
 const library = {
   storage: [],
 
-  add: function(book) {
+  bookCount: 0,
+
+  add: function(title, author, pages, read) {
+    this.bookCount++;
+    const book = new Book(title, author, pages, read, this.bookCount)
     this.storage.push(book);
   },
 
   display: function() {
     for (const book of this.storage) {
       const bookNode = document.createElement('div');
-      bookNode.textContent = book.info();
+      bookNode.setAttribute('data-book-id', `${book.id}`)
+      bookNode.insertAdjacentHTML('beforeend', `
+       <h2>${book.title}</h2>
+       <p>By: ${book.author}</p>
+       <p>Pages: ${book.pages}</p>
+       <label for="read${book.id}">Read:</label>
+       <input type="checkbox" name="read${book.id}" id="read${book.id}">`
+      );
+
       mainNode.insertBefore(bookNode, addButton)
     }
   }
 }
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read
+  this.read = read,
+  this.id = id
 }
 
 Book.prototype.info = function() {
@@ -29,14 +51,9 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}` 
 }
 
-const hbc = new Book('Hbc', 'Wojciech Drewniak', '457', false)
-const tale = new Book('A Tale of Two Cities', 'Charles Dickens', '448', false)
-const One = new Book('1984', 'George Orwell', '352', true)
-const Fahrenheit = new Book('Fahrenheit 451', 'Ray Bradbury', '227', false)
 
+library.add('A Tale of Two Cities', 'Charles Dickens', '448', false)
+library.add('1984', 'George Orwell', '352', true)
+library.add('Fahrenheit 451', 'Ray Bradbury', '227', false)
 
-library.add(hbc);
-library.add(tale);
-library.add(One);
-library.add(Fahrenheit);
 library.display();
