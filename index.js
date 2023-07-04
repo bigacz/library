7a3ac2c23a3b13ab337215edb5215cef27de876b
bigacz <sbigacz@gmail.com>
2023-07-04 20:44:14 +0200

    Fix add button

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ const mainNode = document.querySelector('main');
 const addButton = document.getElementById('add-button');
 const addForm = document.getElementById('form-add');
 const page = document.getElementById('page');
+const submitButton = document.getElementById('submit');
 
-addForm.style.display = "none";
+
+const form = {
+  getValues: function(inputs) {
+    let values = [];
+    inputs.forEach(element => {
+      if(element.getAttribute('type') === 'checkbox') {
+        values.push(element.checked);
+      } else {
+        values.push(element.value);
+      }
+    })
+    return values
+  },
+
+  toggle: function() {
+    const form = document.getElementById('form-add');
+    form.style.display = (form.style.display == 'none') ? 'block' : 'none';
+  }
+}
+
+form.toggle();
+
+submitButton.addEventListener('click', (event) => {
+  const inputs = document.querySelectorAll('#form-add input');
+  library.add(...form.getValues(inputs));
+  form.toggle();
+  event.preventDefault();
+})
 
 addButton.addEventListener('click', (e) => {
-  addForm.style.display = "block";
+  form.toggle();
   page.disabled = true;
 })
 
@@ -16,21 +44,33 @@ const library = {
   bookCount: 0,
 
   add: function(title, author, pages, read) {
+    this.display();
     this.bookCount++;
     const book = new Book(title, author, pages, read, this.bookCount)
     this.storage.push(book);
+    this.display();
+  },
+
+  detachAll: function() {
+    while(mainNode.childElementCount > 1) {
+      mainNode.removeChild(mainNode.firstChild);
+    }
   },
 
   display: function() {
+    this.detachAll();
     for (const book of this.storage) {
       const bookNode = document.createElement('div');
+
+      isChecked = book.read ? 'checked' : '';
+
       bookNode.setAttribute('data-book-id', `${book.id}`)
       bookNode.insertAdjacentHTML('beforeend', `
        <h2>${book.title}</h2>
        <p>By: ${book.author}</p>
        <p>Pages: ${book.pages}</p>
        <label for="read${book.id}">Read:</label>
-       <input type="checkbox" name="read${book.id}" id="read${book.id}">`
+       <input type="checkbox" name="read${book.id}" id="read${book.id}"  ${isChecked}>`
       );
 
       mainNode.insertBefore(bookNode, addButton)
@@ -51,9 +91,11 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}` 
 }
 
-
 library.add('A Tale of Two Cities', 'Charles Dickens', '448', false)
 library.add('1984', 'George Orwell', '352', true)
+
+
+
 library.add('Fahrenheit 451', 'Ray Bradbury', '227', false)
 
-library.display();
+
