023434c20dc321e7718d36ae20665b6db819a3e1
bigacz <sbigacz@gmail.com>
2023-10-29 22:30:23 +0100

    add eslint and prettier

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const addButton = document.getElementById('add-button');
 const page = document.getElementById('page');
 const submitForm = document.getElementById('submit');
 const cancelForm = document.getElementById('cancel');
-const readForm = document.getElementById('form-read')
+const readForm = document.getElementById('form-read');
 
 class Book {
-
   constructor(title, author, pages, read, id) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read,
-    this.id = id
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
   }
 }
 
@@ -20,56 +19,56 @@ const library = {
   storage: [],
   bookCount: 0,
 
-  add: function(title, author, pages, read) {
-    const book = new Book(title, author, pages, read, (this.bookCount));
+  add(title, author, pages, read) {
+    const book = new Book(title, author, pages, read, this.bookCount);
 
     this.storage.push(book);
-    this.bookCount++;
+    this.bookCount += 1;
     this.display();
   },
 
-
-  remove: function(id) {
-    const bookNode = mainNode.querySelector(`div[data-book-id="${id}"]`)
+  remove(id) {
+    const bookNode = mainNode.querySelector(`div[data-book-id="${id}"]`);
     const index = this.findstorageId(id);
 
     bookNode.remove();
     this.storage.splice(index, 1);
   },
 
-  findstorageId: function(id) {
-    return this.storage.findIndex((e) => e.id == id);
+  findstorageId(id) {
+    return this.storage.findIndex((e) => +e.id === +id);
   },
 
-  createNode: function(book) {
+  createNode(book) {
     const bookNode = document.createElement('div');
-    const isChecked = book.read ? 'checked' : '';
-    let pages = book.pages ? `${book.pages} pages` : 'pages not specified';
-    let author = book.author ? `${book.author}` : 'author not specified';
-    
+    const pages = book.pages ? `${book.pages} pages` : 'pages not specified';
+    const author = book.author ? `${book.author}` : 'author not specified';
+
     bookNode.setAttribute('data-book-id', `${book.id}`);
     bookNode.classList.add('book-node');
-    bookNode.insertAdjacentHTML('beforeend', `
+    bookNode.insertAdjacentHTML(
+      'beforeend',
+      `
       <h2>${book.title}</h2>
       <p>${author}</p>
       <p>${pages}</p>
       <button class="read-false" type="button">Read</button>
       <button class="removeButton" type="button">Remove</button>`
     );
-    
+
     mainNode.appendChild(bookNode);
 
-    const removeButton = bookNode.querySelector('button.removeButton')
-    const readButton = bookNode.querySelector('button.read-false')
+    const removeButton = bookNode.querySelector('button.removeButton');
+    const readButton = bookNode.querySelector('button.read-false');
 
-    if(!!book.read) {
-      readButton.classList.toggle('read-true')
+    if (book.read) {
+      readButton.classList.toggle('read-true');
     }
 
     removeButton.addEventListener('click', (e) => {
-      id = e.currentTarget.parentElement.getAttribute('data-book-id');
+      const id = e.currentTarget.parentElement.getAttribute('data-book-id');
       library.remove(id);
-    })
+    });
 
     readButton.addEventListener('click', (e) => {
       const id = e.currentTarget.parentElement.getAttribute('data-book-id');
@@ -77,101 +76,100 @@ const library = {
 
       this.storage[storageId].read = !this.storage[storageId].read;
 
-      readButton.classList.toggle('read-true')
-    })
+      readButton.classList.toggle('read-true');
+    });
   },
 
-  detachAll: function() {
-    while(mainNode.childElementCount > 1) {
+  detachAll() {
+    while (mainNode.childElementCount > 1) {
       mainNode.removeChild(mainNode.lastChild);
     }
   },
 
-  display: function() {
+  display() {
     this.detachAll();
     for (const book of this.storage) {
       this.createNode(book);
     }
-  }
-}
+  },
+};
 
-const backdrop =  document.getElementById('backdrop');
-backdrop.addEventListener('click', e => {
-
+const backdrop = document.getElementById('backdrop');
+backdrop.addEventListener('click', () => {
   form.toggle();
-})
+});
 
 const form = {
   inputs: document.querySelectorAll('#form input, #form-read'),
   formNode: document.getElementById('form'),
 
-  getValues: function() {
-    let values = [];
-    this.inputs.forEach(element => {
-      if(element.id === 'form-read') {
-        let toPush = element.value === 'false' ? false : true;
-        values.push(toPush)
+  getValues() {
+    const values = [];
+    this.inputs.forEach((element) => {
+      if (element.id === 'form-read') {
+        const toPush = element.value !== 'false';
+        values.push(toPush);
       } else {
         values.push(element.value);
       }
-    })
-    return values
+    });
+    return values;
   },
 
-  toggle: function() {
-    let state = (this.formNode.style.display == 'none') ? 'block' : 'none';
+  toggle() {
+    const state = this.formNode.style.display === 'none' ? 'block' : 'none';
     this.clearInputs();
     this.formNode.style.display = state;
     backdrop.style.display = state;
   },
 
-  checkValidity: function() {
+  checkValidity() {
     let valid = true;
-    this.inputs.forEach(element => {
+    this.inputs.forEach((element) => {
       if (!element.checkValidity()) {
         valid = false;
       }
-    })
-    return valid
+    });
+    return valid;
   },
 
-  clearInputs: function() {
-    this.inputs.forEach(element => {
-      if(element.tagName == 'BUTTON') {
+  clearInputs() {
+    this.inputs.forEach((element) => {
+      if (element.tagName === 'BUTTON') {
         element.value = 'false';
         element.classList.remove('read-true');
       } else {
         element.value = '';
       }
-    })
+    });
   },
-}
+};
 
 addButton.addEventListener('click', () => {
   form.toggle();
   page.disabled = true;
-})
+});
 
 readForm.addEventListener('click', (event) => {
   event.currentTarget.classList.toggle('read-true');
-  event.currentTarget.value = event.currentTarget.value == "false" ? true : false
-})
+  event.currentTarget.value = event.currentTarget.value === 'false';
+});
 
 submitForm.addEventListener('click', (event) => {
-  if(form.checkValidity()) {
+  if (form.checkValidity()) {
     library.add(...form.getValues());
     form.toggle();
     event.preventDefault();
   }
-})
+});
 
-cancelForm.addEventListener('click', (e) => {
+cancelForm.addEventListener('click', () => {
   form.toggle();
-})
+});
 
 form.toggle();
 
-library.add('A Tale of Two Cities', 'Charles Dickens', '448', false)
-library.add('1984', 'George Orwell', '352', true)
-library.add('Fahrenheit 451', 'Ray Bradbury', '227', false)
-library.add('Brave New World', 'Aldous Huxley', '231', false)
+library.add('A Tale of Two Cities', 'Charles Dickens', '448', false);
+library.add('1984', 'George Orwell', '352', true);
+library.add('Fahrenheit 451', 'Ray Bradbury', '227', false);
+library.add('Brave New World', 'Aldous Huxley', '231', false);
